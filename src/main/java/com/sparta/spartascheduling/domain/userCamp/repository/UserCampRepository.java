001e36c6d634aee2e97307babcb21b98f764c581
kuk1157 <kuk1938@naver.com>
2024-11-23 19:40:58 +0900

    feat: 캠프 신청 - POST - 로그인 정보 없이 임시 기능완료 - 예외처리 완료

--- src/main/java/com/sparta/spartascheduling/domain/userCamp/repository/UserCampRepository.java
+++ src/main/java/com/sparta/spartascheduling/domain/userCamp/repository/UserCampRepository.java
@@ -1,4 +1,9 @@
 package com.sparta.spartascheduling.domain.userCamp.repository;
 
-public class UserCampRepository {
+import com.sparta.spartascheduling.domain.userCamp.entity.UserCamp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCampRepository extends JpaRepository<UserCamp, Long> {
+    // 회원 아이디 없어서 임시로
+    UserCamp findByUserId(Long l);
 }
