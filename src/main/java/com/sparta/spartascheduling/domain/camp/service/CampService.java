a2573b6ac06a706b92a9369e356847f1ab42f258
kuk1157 <kuk1938@naver.com>
2024-11-24 20:11:01 +0900

    refactor: 캠프 신청 예외 수정 및 수정사항 - 정원초과 예외처리 조건 변경 - 남은인원-1 메서드 생성하여 service에서 실행

--- src/main/java/com/sparta/spartascheduling/domain/camp/service/CampService.java
+++ src/main/java/com/sparta/spartascheduling/domain/camp/service/CampService.java
@@ -7,6 +7,7 @@ import com.sparta.spartascheduling.domain.userCamp.entity.UserCamp;
 import com.sparta.spartascheduling.domain.userCamp.repository.UserCampRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class CampService {
     private final UserCampRepository userCampRepository;
     private final UserRepository userRepository;
 
+    @Transactional()
     public void applyForCamp(Long campId) {
         Camp camp = campRepository.findById(campId).orElseThrow(()-> new IllegalArgumentException("캠프가 존재하지 않습니다."));
         User user = userRepository.findById(1L).orElseThrow(()-> new IllegalArgumentException("존재하지 않는 학생입니다.")); // 회원 아이디 임시
@@ -24,10 +26,15 @@ public class CampService {
             throw new IllegalArgumentException("이미 소속된 캠프는 신청할 수 없습니다.");
         }
 
-        if(userCampCheck != null && userCampCheck.getCamp().getRemainCount() >= userCampCheck.getCamp().getMaxCount()){
+        if(userCampCheck != null && userCampCheck.getCamp().getRemainCount() <= 0){
             throw new IllegalArgumentException("정원이 초과되어서 캠프를 신청할 수 없습니다.");
         }
 
+        // camp 엔티티에서 캠프신청될때 남은인원 -1 메서드 실행
+        camp.decreaseRemainCount();
+        campRepository.save(camp); // camp 테이블에 업데이트 진행
+
+        // 캠프 등록
         UserCamp userCamp = UserCamp.of(user, camp);
         userCampRepository.save(userCamp);
     }
