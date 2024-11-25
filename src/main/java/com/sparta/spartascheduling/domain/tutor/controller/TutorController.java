23870ae099c207536c02596f6d4bb8de87cc4d06
gyeonwoo-jerry <gyeonwoo2916@google.com>
2024-11-25 21:24:02 +0900

    - add : @Requestbody 추가

--- src/main/java/com/sparta/spartascheduling/domain/tutor/controller/TutorController.java
+++ src/main/java/com/sparta/spartascheduling/domain/tutor/controller/TutorController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +29,7 @@ public class TutorController {
     }
 
     @PutMapping("/tutor/update-time")
-    public ResponseEntity<TutorResponseDto> updateTime(@Auth AuthUser authUser, @Valid TutorRequest request) {
+    public ResponseEntity<TutorResponseDto> updateTime(@Auth AuthUser authUser, @Valid @RequestBody TutorRequest request) {
         TutorResponseDto response = tutorService.updateTime(authUser, request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
