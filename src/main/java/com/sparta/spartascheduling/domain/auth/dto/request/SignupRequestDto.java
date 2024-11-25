3c1a74c27efb3660c0a644b8803a9d4a7da2d9a0
yokxim2 <yooksi53@gmail.com>
2024-11-25 14:10:45 +0900

    fix: boolean Type 검증 수정 NotBlank -> NotNull, @EnableJpaAuditing

--- src/main/java/com/sparta/spartascheduling/domain/auth/dto/request/SignupRequestDto.java
+++ src/main/java/com/sparta/spartascheduling/domain/auth/dto/request/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.spartascheduling.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,6 @@ public class SignupRequestDto {
 	@NotBlank
 	private String passwordConfirm;
 
-	@NotBlank
+	@NotNull
 	private boolean admin;
 }
