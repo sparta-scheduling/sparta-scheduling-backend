00b5c1745f9b0329f08a0453ad78a79f086601ce
92jy38 <92jy38@gmail.com>
2024-11-25 12:31:25 +0900

    refactor: db와 상호작용하는 중복 확인 메서드 repository에서 처리하도록 수정

--- src/main/java/com/sparta/spartascheduling/domain/camp/repository/CampRepository.java
+++ src/main/java/com/sparta/spartascheduling/domain/camp/repository/CampRepository.java
@@ -7,12 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sparta.spartascheduling.domain.camp.entity.Camp;
 
 public interface CampRepository extends JpaRepository<Camp, Long> {
+	// 중복 여부 확인 메서드
 	boolean existsByNameAndOpenDate(String name, LocalDate openDate);
-
-	// 중복 캠프 확인 메서드 추가
-	default void checkDuplicateCamp(String name, LocalDate openDate) {
-		if (existsByNameAndOpenDate(name, openDate)) {
-			throw new IllegalArgumentException("같은 이름과 시작일의 캠프가 이미 존재합니다.");
-		}
-	}
 }
