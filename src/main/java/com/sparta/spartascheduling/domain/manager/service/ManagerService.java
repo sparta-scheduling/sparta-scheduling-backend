68ae4408321123a8d96660e2e037c5b61ffb13bc
92jy38 <92jy38@gmail.com>
2024-11-25 10:27:47 +0900

    fix: 불필요한 파일 수정

--- src/main/java/com/sparta/spartascheduling/domain/manager/service/ManagerService.java
+++ src/main/java/com/sparta/spartascheduling/domain/manager/service/ManagerService.java
@@ -2,7 +2,6 @@ package com.sparta.spartascheduling.domain.manager.service;
 
 import org.springframework.stereotype.Service;
 
-import com.sparta.spartascheduling.domain.manager.dto.ManagerRequestDto;
 import com.sparta.spartascheduling.domain.manager.repository.ManagerRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +12,4 @@ public class ManagerService {
 
 	private final ManagerRepository managerRepository;
 
-	public void signupManager(ManagerRequestDto requestDto) {
-	}
 }
